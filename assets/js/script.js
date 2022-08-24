ac2a225f4f778fdc32d36d5f4035f7db9000eb7f
Josue Luna <gicg@gmail.com>
2022-08-23 21:29:38 -0400

    Last commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -34,9 +34,6 @@ function getDaily() {
         .catch(error => console.log(error))
 
     const showData = (data) => {
-        console.log(data)
-        console.log(data[0].currentDailyWork)
-
         currentWork.innerHTML = `${data[0].currentDailyWork}hrs`
         previousWork.innerHTML = `Last Day - ${data[0].previousDailyWork}hrs`
 
@@ -64,9 +61,6 @@ function getWeekly() {
         .catch(error => console.log(error))
 
     const showData = (data) => {
-        console.log(data)
-        console.log(data[1].currentWeeklyWork)
-
         currentWork.innerHTML = `${data[1].currentWeeklyWork}hrs`
         previousWork.innerHTML = `Last Week - ${data[1].previousWeeklyWork}hrs`
 
@@ -94,9 +88,6 @@ function getMonthly() {
         .catch(error => console.log(error))
 
     const showData = (data) => {
-        console.log(data)
-        console.log(data[2].currentMonthlyWork)
-
         currentWork.innerHTML = `${data[2].currentMonthlyWork}hrs`
         previousWork.innerHTML = `Last Month - ${data[2].previousMonthlyWork}hrs`
 
@@ -116,77 +107,3 @@ function getMonthly() {
         previousSelfCare.innerHTML = `Last Month - ${data[2].previousMonthlySelfCare}hrs`
     }
 }
-// function getDaily() {
-//     fetch(dataJson)
-//         .then(res => res.json())
-//         .then(data => {
-//             dailyTable(data)
-//         })
-
-//     function dailyTable(data) {
-//         for (let value of data) {
-
-//             currentWork.innerHTML = `${data[0].currentDailyWork} hrs`
-//             previousWork.innerHTML = `Last Day - ${data[0].previousDailyWork} hrs`
-
-//             currentPlay.innerHTML = `${data[0].currentDailyPlay} hrs`
-//             previousPlay.innerHTML = `Last Day - ${data[0].previousDailyPlay} hrs`
-
-//             currentStudy.innerHTML = `${data[0].currentDailyStudy} hrs`
-//             previousStudy.innerHTML = `Last Day - ${data[0].previousDailyStudy} hrs`
-
-//             currentExercise.innerHTML = `${data[0].currentDailyExercise} hrs`
-//             previousExercise.innerHTML = `Last Day - ${data[0].previousDailyExercise} 1hrs`
-
-//             currentSocial.innerHTML = `${data[0].currentDailySocial} hrs`
-//             previousSocial.innerHTML = `Last Day - ${data[0].previousDailySocial} hrs`
-
-//             currentSelfCare.innerHTML = `${data[0].currentDailySelfCare} hrs`
-//             previousSelfCare.innerHTML = `Last Day - ${data[0].previousDailySelfCare} hrs`
-
-
-//             //console.log(value)
-//             // console.log(value.daily)
-//             //console.log(data[0].currentDailyWork)
-//         }
-//     }
-// }
-
-// function getWeekly() {
-//     fetch(dataJson)
-//         .then(res => res.json())
-//         .then(data => {
-//             //console.log(data)
-//             weeklyTable(data)
-//         })
-
-//     function weeklyTable(data) {
-//         // console.log(data)
-//         //currentWork.innerHTML = ''
-//         for (let value of data) {
-
-//             currentWork.innerHTML = `${data[1].currentWeeklyWork} hrs`
-//             previousWork.innerHTML = `Last Week - ${data[1].previousWeeklyWork} hrs`
-
-//             currentPlay.innerHTML = `${data[1].currentWeeklyPlay} hrs`
-//             previousPlay.innerHTML = `Last Week - ${data[1].previousWeeklyPlay} hrs`
-
-//             currentStudy.innerHTML = `${data[1].currentWeeklyStudy} hrs`
-//             previousStudy.innerHTML = `Last Week - ${data[1].previousWeeklyStudy} hrs`
-
-//             currentExercise.innerHTML = `${data[1].currentWeeklyExercise} hrs`
-//             previousExercise.innerHTML = `Last Week - ${data[1].previousWeeklyExercise} 1hrs`
-
-//             currentSocial.innerHTML = `${data[1].currentWeeklySocial} hrs`
-//             previousSocial.innerHTML = `Last Week - ${data[1].previousWeeklySocial} hrs`
-
-//             currentSelfCare.innerHTML = `${data[1].currentWeeklySelfCare} hrs`
-//             previousSelfCare.innerHTML = `Last Week - ${data[1].previousDailySelfCare} hrs`
-
-
-//             //console.log(value)
-//             // console.log(value.daily)
-//             //console.log(data[0].currentDailyWork)
-//         }
-//     }
-// }
